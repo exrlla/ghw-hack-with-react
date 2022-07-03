aa892f4b68a82563c4ed026d5f0c5e2b63dc30e7
estrella <epopo2@illinois.edu>
2022-07-03 14:23:31 -0500

    add nav bar

---
+++ my-app/src/pages/App.js
@@ -0,0 +1,12 @@
+import logo from '../logo.svg';
+import '../stylesheets/App.css';
+import Navbar from '../Components/Navbar';
+function App() {
+  return (
+    <div className="App">
+    <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
