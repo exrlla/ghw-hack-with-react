aed35a97ac8261c0fdb1d9661ed8c2c25884ed12
estrella <epopo2@illinois.edu>
2022-07-03 19:47:06 -0500

    add navbar from stream

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'; 
+import '../stylesheets/App.css';
+
+function Navbar() {
+    const [showLinks, setShowLinks] = useState(false);
+    return (
+        <nav className="navbar">
+            <div className='leftSide'>
+                <div className='links' id={showLinks ? "hidden" : ""}>
+                    <a href='/'>Home</a>
+                    <a href='/about'>About</a>
+                    <a href='/contact'>Contact</a>
+                </div>
+                <button onClick={() => setShowLinks(!showLinks)}>Open </button>
+            </div>
+           
+            <div className='rightSide'>
+                <input type="text" placeholder='hey hackers!'></input>
+                <button className='search-button'>Search</button>
+            </div>
+           </nav>
+    );
+}
+
+export default Navbar;
