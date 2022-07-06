a9d0f090cb9979144b437244bb491710528376d6
Zeyu Yao <novodoodle@gmail.com>
2022-07-06 10:36:20 +0800

    Add comments and update README

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+/** This is the main file for you to develop your app!
+ * Everything inside the App return statement will be rendered to the DOM.
+*/
+
 function App() {
+
+  /** If you want to create a component, create it here and call it inside the return statement.
+   * For example, if you want to create a component called "MyComponent", you can create it here and call it "MyComponent":
+   * const MyComponent = () => {
+   *  return <div>MyComponent</div>
+   * }
+   * Then, in the return statement, you can call it like this:
+   * <MyComponent />
+   * Learn more about components: https://reactjs.org/docs/components-and-props.html
+  */
+
   return (
     <div className="App">
       <header className="App-header">
--- my-app/src/reportWebVitals.js
+++ my-app/src/reportWebVitals.js
@@ -1,3 +1,7 @@
+/** By default, Create React App includes a performance relayer for you to analyze your application's performance. 
+ * Learn more about web vitals: https://create-react-app.dev/docs/measuring-performance/
+*/
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
